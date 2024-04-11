e1bc1d94ca618c56c090d165b37b860b97111f85
Brayan YL <106005521+BrayanYL25@users.noreply.github.com>
2024-04-11 15:07:32 -0500

    feat: all app created

---
+++ src/components/CoSo.jsx
@@ -0,0 +1,8 @@
+export default function ControlSection () {
+  return (
+    <div className='flex gap-2'>
+      <button className='p-2 rounded-xl border-2 border-steelBlue'><img src='/translate-app/sound_max_fill.svg' alt='Play song button' /></button>
+      <button className='p-2 rounded-xl border-2 border-steelBlue'><img src='/translate-app/Copy.svg' alt='Copy text button' /></button>
+    </div>
+  )
+}
